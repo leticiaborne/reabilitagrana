f2c872c709e8209d497199c44a9d83f8030916a4
Lucas Rafael <3lucasrs@gmail.com>
2024-06-29 23:56:33 -0300

    updated server host

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   host: "10.0.0.103",
-  // },
+  server: {
+    host: "localhost",
+  },
 });
